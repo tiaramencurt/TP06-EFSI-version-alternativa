8dc8e095b7a9bd7bbbf16b369257db1b8fdbe2fd
MAICA TRUTNER KOBRYNIEC <49192000@ort>
2026-05-12 11:46:09 -0300

    m

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import './Header.css'
 function Header() {
    return (
       <header className='header'>
-         <h1>Catstagram</h1>
+         <h1>Instagram</h1>
 
          <input
             type='text'
